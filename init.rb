6efb18eea1f49637a70e6ea5cb314a5550cb1f40
Keiji Yoshida <km.8k6ce+github@gmail.com>
2017-03-26 15:07:40 +0900

    Generate plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :m17n_tagging do
+  name 'Multilingual Tagging plugin'
+  author 'kei-g'
+  description 'Multilingual tagging support'
+  version '0.0.1'
+  url 'https://github.com/kei-g/redmine_m17n_tagging'
+  author_url 'https://github.com/kei-g'
+end
