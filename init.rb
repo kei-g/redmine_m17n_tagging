43d8eac6517ed4c2decd990b807c785e9b332583
Keiji Yoshida <km.8k6ce+github@gmail.com>
2017-04-02 02:24:03 +0900

    Create multilingual tags controller and views

--- init.rb
+++ init.rb
@@ -9,4 +9,12 @@ Redmine::Plugin.register :m17n_tagging do
   version '0.0.1'
   url 'https://github.com/kei-g/redmine_m17n_tagging'
   author_url 'https://github.com/kei-g'
+
+  menu :project_menu,
+       :multilingual_tags,
+       { action: :index, controller: :multilingual_tags },
+       caption: :multilingual_tags,
+       param: :project_id
+
+  permission :multilingual_tagging, { multilingual_tags: [ :create, :destroy, :edit, :index, :new, :show, :update ] }, public: true
 end
